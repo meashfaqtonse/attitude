9d54be57d092bd85119e3f7fad90cd78f02ea13c
ashfaqtonse <ashfaq.dlxsphp@gmail.com>
2015-09-10 15:03:28 +0530

    Change auth.php

--- auth.php
+++ auth.php
@@ -2,7 +2,7 @@
 	//Start session
 	session_start();
 	
-	//Check whether the session variable SESS_MEMBER_ID is present or not
+	//Check whether the session variable SESS_MEMBER_ID is present or not as
 	if(!isset($_SESSION['SESS_MEMBER_ID']) || (trim($_SESSION['SESS_MEMBER_ID']) == '')) {
 		header("location: access-denied.php");
 		exit();
